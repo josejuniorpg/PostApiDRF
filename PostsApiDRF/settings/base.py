f4c9ef5f51557c0cc61623c051c903d6c90797de
josejuniorpg <yunipgx@protonmail.com>
2024-08-05 00:05:10 -0600

    Install DRF

--- PostsApiDRF/settings/base.py
+++ PostsApiDRF/settings/base.py
@@ -31,6 +31,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Third-party apps
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
