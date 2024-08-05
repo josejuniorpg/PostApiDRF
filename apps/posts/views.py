ac7c69e15572cc71974c87a984176150a5f86fb4
josejuniorpg <yunipgx@protonmail.com>
2024-08-05 00:28:20 -0600

    Post app, permisions, serializes, etc.

--- apps/posts/views.py
+++ apps/posts/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from rest_framework import viewsets  # new
+from rest_framework.permissions import IsAdminUser  # new
 
-# Create your views here.
+from .models import Post
+from .permissions import IsAuthorOrReadOnly
+from .serializers import PostSerializer, UserSerializer
+
+
+class PostViewSet(viewsets.ModelViewSet):  # new
+    permission_classes = (IsAuthorOrReadOnly,)
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+
+class UserViewSet(viewsets.ModelViewSet):  # new
+    permission_classes = [IsAdminUser]  # only for admin users
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
